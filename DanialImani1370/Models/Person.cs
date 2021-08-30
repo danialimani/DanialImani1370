451e6ad2e2e4dde189902f20ab0b4f0ac86cf984
Danial Imani <danialimani1370@outlook.com>
2021-08-30 15:52:14 +0430

    Add project files.

---
+++ DanialImani1370/Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DanialImani1370.Models
+{
+    public class Person
+    {
+        [DisplayName("شماره")]
+        public int Id { get; set; }
+        [DisplayName("نام")]
+        public string FirstName { get; set; }
+        [DisplayName("نام خانوادگی")]
+        public string LastName { get; set; }
+        [DisplayName("سن")]
+        public int Age { get; set; } 
+        [DisplayName("سمت")]
+        public string Job { get; set; }
+        [DisplayName("جنسیت")]
+        public string Sex { get; set; }
+        [DisplayName("تحصیلات")]
+        public string Education { get; set; }
+        [DisplayName("تاریخ تولد")]
+        public string BrithDate { get; set; }
+
+    }
+}
