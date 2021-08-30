1f0ffa7d0ed2ab3f07e8b7e29c1ab33596d2f187
Danial Imani <danialimani1370@outlook.com>
2021-08-30 19:01:23 +0430

    Table-option

--- DanialImani1370/Models/TableOption.cs
+++ DanialImani1370/Models/TableOption.cs
@@ -26,6 +26,9 @@ namespace DanialImani1370.Models
         public string TdEdit { get; set; }
         public string TdDelete { get; set; }
         public string Atag { get; set; }
+        public string ThAtag { get; set; }
+        public string ExcelLink { get; set; }
+        public string ColumnsLink { get; set; }
         public TableOption()
         {
             TblContainer = "tbl-container";
@@ -36,15 +39,17 @@ namespace DanialImani1370.Models
             TblThead = "tbl-thead";
             TblTbody = "tbl-tbody";
             InputSerach = "form-control txt-box-serach";
-            ThDetail = "text-center text-white";
-            ThDelete = "text-center text-white";
-            ThEdit = "text-center text-white";
+            ThDetail = "text-center text-white fs-15";
+            ThDelete = "text-center text-white fs-15";
+            ThEdit = "text-center text-white fs-15";
             TdDetail = "text-center ";
             TdDelete = "text-center ";
             TdEdit = "text-center t";
             TblFooter = "tbl-footer";
-            Atag = "text-white";
-            Th = "text-white";
+            ThAtag = "text-white fs-15";
+            ExcelLink = "text-white";
+            ColumnsLink = "text-white";
+            Th = "text-white fs-15";
         }
     }
 }
